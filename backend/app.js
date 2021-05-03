dc40270de66f9b4c0d02afdde8a8a1a3c3c66842
Victoria Kee <victoriakee95@gmail.com>
2021-05-03 13:44:15 -0500

    Get our current year

--- backend/app.js
+++ backend/app.js
@@ -25,3 +25,9 @@ const months = [
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2021, 5, 24, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+
+giveaway.textContent = `giveaway ends on ${year}`
